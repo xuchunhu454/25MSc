eee1136a2006ecd9e39be9fc9fa75f15833a9810
xh824 <xh824@ic.ac.uk>
2025-05-17 15:45:34 +0100

    1

---
+++ examples_mlp/firmware/weights/b11.h
@@ -0,0 +1,15 @@
+//Numpy array shape [5]
+//Min -0.132539346814
+//Max 0.051749393344
+//Number of zeros 0
+
+#ifndef B11_H_
+#define B11_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b11[5];
+#else
+model_default_t b11[5] = {-0.1325393468, 0.0350524858, 0.0479941405, 0.0517493933, -0.0105751036};
+#endif
+
+#endif
